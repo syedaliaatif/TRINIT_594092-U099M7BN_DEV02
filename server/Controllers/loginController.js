c773ab78b6e31d1bb902b571662550ad4f1d2b47
inaakbar <95181878+inaakbar@users.noreply.github.com>
2023-03-14 19:03:20 +0530

    updated login controllers

--- server/Controllers/loginController.js
+++ server/Controllers/loginController.js
@@ -1,4 +1,6 @@
-
+const Login = require("../Model/LoginModel");
+const { hashPassword, comparePasswords } = require("../utils/hashPassword");
+const generateAuthToken = require("../utils/generateAuthToken");
 
 const getUsers = async (req,res,next)=> {
     try{
@@ -9,26 +11,96 @@ const getUsers = async (req,res,next)=> {
     }
 }
 
-const registerUser = async (req,res,next)=>{
-    try{
-        const {name, email, password} = req.body
-        if(!(name && email && password)){
-            return res.status(400).send("All inputs are required")
-        }
-
-        if(userExists){
-            return res.status(400).json({error: "user exists"})
-        }else{
-            const user = await Login.create({
-                name,email : email.toLowerCase(),
-                password: password
-            });
-            res.status(201).send(login)
-        }
-
-    }catch(err){
-        next(err)
+const registerUser = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!(name && email && password)) {
+      return res.status(400).send("All inputs are required");
     }
-}
-module.exports= {getUsers,registerUser}
+
+    const userExists = await Login.findOne({ email });
+    if (userExists) {
+      return res.status(400).send("user exists");
+    } else {
+      const hashedPassword = hashPassword(password);
+      const user = await Login.create({
+        name,
+        email: email.toLowerCase(),
+        password: hashedPassword,
+      });
+      res
+        .cookie(
+          "access_token",
+          generateAuthToken(
+            user._id,
+            user.name,
+            user.email,
+          ),
+          {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+          }
+        )
+        .status(201)
+        .json({
+          success: "User created",
+          userCreated: {
+            _id: user._id,
+            email: user.email,
+          },
+        });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password, doNotLogout } = req.body;
+    if (!(email && password)) {
+      return res.status(400).send("All inputs are required");
+    }
+
+    const user = await Login.findOne({ email }).orFail();
+    if (user && comparePasswords(password, user.password)) {
+      let cookieParams = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+      };
+
+      if (doNotLogout) {
+        cookieParams = { ...cookieParams, maxAge: 1000 * 60 * 60 * 24 * 7 }; // 1000=1ms
+      }
+
+      return res
+        .cookie(
+          "access_token",
+          generateAuthToken(
+            user._id,
+            user.name,
+            user.email,
+          ),
+          cookieParams
+        )
+        .json({
+          success: "user logged in",
+          userLoggedIn: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            doNotLogout,
+          },
+        });
+    } else {
+      return res.status(401).send("wrong credentials");
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports= {getUsers,registerUser, loginUser};
 
