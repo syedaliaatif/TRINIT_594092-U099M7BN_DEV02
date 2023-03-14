cd7c8bc5f795143873d6c28aeb71df616b4dd836
inaakbar <95181878+inaakbar@users.noreply.github.com>
2023-03-14 19:39:10 +0530

    Add files via upload

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,65 +1,28 @@
-import React from 'react';
-import {MDBContainer, MDBCol, MDBRow, MDBBtn, MDBIcon, MDBInput, MDBCheckbox } from 'mdb-react-ui-kit';
-import "../App.css";
+import RegisterPageComponent from "../components/RegisterPageComponent";
+import axios from "axios";
+//import { useDispatch } from "react-redux";
+//import { setReduxUserState } from "../redux/actions/userActions";
 
-function Register() {
+const registerUserApiRequest = async (name , email, password) => {
+  const { data } = await axios.post("/api/users/register", {
+    name,
+    email,
+    password,
+  });
+  sessionStorage.setItem("userInfo", JSON.stringify(data.userCreated));
+  if (data.success === "User created") window.location.href = "/user";
+  return data;
+};
 
+const Register = () => {
+  //const reduxDispatch = useDispatch();
   return (
-    <MDBContainer fluid className="p-3 my-5 h-custom">
-
-      <MDBRow>
-
-        <MDBCol col='10' md='6'>
-          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" class="img-fluid" alt="Sample image" />
-        </MDBCol>
-
-        <MDBCol col='4' md='6'>
-
-          <div className="d-flex flex-row align-items-center justify-content-center">
-
-            <p className="lead fw-normal mb-0 me-3">Register with</p>
-
-            <MDBBtn floating size='md' tag='a' className='me-2'>
-              <MDBIcon fab icon='facebook-f' />
-            </MDBBtn>
-
-            <MDBBtn floating size='md' tag='a'  className='me-2'>
-              <MDBIcon fab icon='twitter' />
-            </MDBBtn>
-
-            <MDBBtn floating size='md' tag='a'  className='me-2'>
-              <MDBIcon fab icon='linkedin-in' />
-            </MDBBtn>
-
-          </div>
-
-          <div className="divider d-flex align-items-center my-4">
-           
-          </div>
-
-          <MDBInput wrapperClass='mb-4' label='Name' id='formControlLg' type='email' size="lg"/>
-          <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
-          <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
-          <MDBInput wrapperClass='mb-4' label='Retype password' id='formControlLg' type='password' size="lg"/>
-
-          <div className="d-flex justify-content-between mb-4">
-            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
-           
-          </div>
-
-          <div className='text-center text-md-start mt-4 pt-2'>
-            <MDBBtn className="mb-0 px-5" size='lg'>Register</MDBBtn>
-            <p className="small fw-bold mt-2 pt-1 mb-2">Don't have an account? <a href="#!" className="link-danger">Register</a></p>
-          </div>
-
-        </MDBCol>
-
-      </MDBRow>
-
-      
-
-    </MDBContainer>
+    <RegisterPageComponent
+      registerUserApiRequest={registerUserApiRequest}
+      // reduxDispatch={reduxDispatch}
+      // setReduxUserState={setReduxUserState}
+    />
   );
-}
+};
 
 export default Register;
